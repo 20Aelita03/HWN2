f8a7072ece8adde47b54c79c35f7b7b6d53142ce
aires <airesheva@mail.ru>
2023-04-13 16:50:15 +0300

    commit 2

---
+++ src/Bicycle.java
@@ -0,0 +1,34 @@
+public class Bicycle extends ServiceStation implements Service{
+    public Bicycle(String modelName,int wheelsCount){
+        if (modelName.isBlank()|| modelName.isEmpty()){
+            this.modelName = "model";
+        } else {
+            this.modelName = modelName;
+        }
+        this.wheelsCount = Math.abs(wheelsCount);
+    }
+
+    protected void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public void service() {
+        System.out.println("Поменяли покрышки");
+    }
+}
---
+++ src/ServiceStation.java
@@ -0,0 +1,67 @@
+import java.util.Objects;
+
+public class ServiceStation{
+    protected String modelName;
+    protected int wheelsCount;
+    protected void check(Car car, Bicycle bicycle, Truck truck) {
+        if (car != null) {
+            System.out.println("Обслуживаем " + car.modelName);
+            for (int i = 0; i < car.wheelsCount; i++) {
+                car.updateTyre();
+            }
+            car.checkEngine();
+            car.service();
+        } else if (truck != null) {
+            System.out.println("Обслуживаем " + truck.modelName);
+            for (int i = 0; i < truck.wheelsCount; i++) {
+                truck.updateTyre();
+            }
+            truck.checkEngine();
+            truck.checkTrailer();
+            truck.service();
+        } else if (bicycle != null) {
+            System.out.println("Обслуживаем " + bicycle.modelName);
+            for (int i = 0; i < bicycle.wheelsCount; i++) {
+                bicycle.updateTyre();
+            }
+            bicycle.service();
+        }
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ServiceStation that = (ServiceStation) o;
+        return wheelsCount == that.wheelsCount && Objects.equals(modelName, that.modelName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(modelName, wheelsCount);
+    }
+
+    @Override
+    public String toString() {
+        return "ServiceStation{" +
+                "modelName='" + modelName + '\'' +
+                ", wheelsCount=" + wheelsCount +
+                '}';
+    }
+}
